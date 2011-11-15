0cb91426b729662c6aed934ed833d95faa46290c
Pedro Nariyoshi <pedro.nariyoshi@gmail.com>
2011-11-15 18:31:54 -0200

    IMU pronto para testar e debugar

--- Controle/src/main.c
+++ Controle/src/main.c
@@ -28,7 +28,9 @@ __CRP const unsigned int CRP_WORD = CRP_NO_CRP ;
 #include "timer16.h"
 #include "gpio.h"
 #include "type.h"
+#include "debug.h"
 
+// Libraries
 #include "PID.h"
 #include "I2C.h"
 #include "ITG3200.h"
@@ -45,17 +47,17 @@ __CRP const unsigned int CRP_WORD = CRP_NO_CRP ;
 
 #define Tamostragem 100             // O tempo de amostragem em ms
 
-typedef struct IMU_sigs_t {
+/*typedef struct IMU_sigs_t {
     int accel_x, accel_y, accel_z;  // Sinal tridimensional do acelerômetro
     int giro_x, giro_y, giro_z;     // Sinal tridimensional da IMU
-}* IMU_sigs_t;
+}* IMU_sigs_t; */
 
 typedef struct nav_params_t {
 	int pitch, roll, yaw;  // Ângulos de Euler, variando de -128 a 127
 	int throttle;          // Potência total dos motores, variando de 0 a 255
 }* nav_params_t;
 
-IMU_sigs_t IMU_sigs;
+//IMU_sigs_t IMU_sigs;
 nav_params_t nav_params;
 
 pid_data_t pid_angles[3];
@@ -63,7 +65,7 @@ pid_data_t pid_pitch;
 pid_data_t pid_roll;
 pid_data_t pid_yaw;
 
-IMU_sigs_t init_IMU_sigs() { return malloc(sizeof(struct IMU_sigs_t)); }
+//IMU_sigs_t init_IMU_sigs() { return malloc(sizeof(struct IMU_sigs_t)); }
 nav_params_t init_nav_params() { return malloc(sizeof(struct nav_params_t)); }
 
 uint8_t still_running;
@@ -113,30 +115,39 @@ void le_nav(nav_params_t params)
 
 int main(void)
 {
-    uint8_t i;
+	uint8_t i;
 
 	inicializa();
 
 	//Loop principal
 	while(1) {
-	    still_running = TRUE;
-		//Lê sinais dos sensores
-		//le_IMU();
+	still_running = TRUE;
+	//Lê sinais dos sensores
+	le_IMU();
+	Accel_GetX();
+	Accel_GetY();
+	Accel_GetZ();
+	Gyro_GetX();
+	Gyro_GetY();
+	Gyro_GetZ();
+	
+	debug_puts("IMU GX: %1.3f GY: %1.3f GZ: %1.3f\n",Gyro_Xr,Gyro_Yr,Gyro_Zr);
+	debug_puts("IMU AX: %1.3f AY: %1.3f AZ: %1.3f\n",Accel_Xr,Accel_Yr,Accel_Zr);
 
-		//Lê parâmetros de navegação
-		//le_nav();
+	//Lê parâmetros de navegação
+	//le_nav();
 
-	    //Realiza o processamento PID
-	    for(i = 0; i < 3; ++i) {
-	        pid_compute(pid_angles[i]);
-	    }
+	//Realiza o processamento PID
+	for(i = 0; i < 3; ++i) {
+		pid_compute(pid_angles[i]);
+	}
 
-	    //Envia as rotações dos motores aos ESCs
-	    //envia_rotacoes();
+	//Envia as rotações dos motores aos ESCs
+	//envia_rotacoes();
 
-	    still_running = FALSE;
-	    /* Go to sleep to save power between timer interrupts */
-	    __WFI();
+	still_running = FALSE;
+	/* Go to sleep to save power between timer interrupts */
+		__WFI();
 	}
 
 	// Enter an infinite loop, just incrementing a counter
